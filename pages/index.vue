30e5ea6db5a460074aa5977f0135edc64c03e599
Jordy Pereira <dev@jordypereira.be>
2024-06-03 21:50:05 +0200

    s

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Main Page</h1>
+    <NuxtLink to="/page1">Go to Page 1</NuxtLink>
+    <NuxtLink to="/page2">Go to Page 2</NuxtLink>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+
+</style>
