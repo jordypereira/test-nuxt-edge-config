d3d06978d0ae54b11946be52a058991e10b30c9b
Jordy Pereira <dev@jordypereira.be>
2024-06-03 21:46:14 +0200

    chore: Update middleware to set variant cookie

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,11 @@ import { get } from '@vercel/edge-config';
 
 export const config = { matcher: '/api/welcome' };
 
-export async function middleware(request: Request): Response {
+export async function middleware (request: Request) {
+  // Simple A/B test logic
+  const variant = Math.random() < 0.5 ? 'A' : 'B';
+
+  // Set a cookie to persist the variant for the user
   const random = Math.random();
   console.log("🚀 ~ eventHandler ~ random:", random)
   const random50Percent = random < 0.5;
@@ -10,16 +14,27 @@ export async function middleware(request: Request): Response {
   const showPlpHeader = random50Percent ? await get('greeting') : false;
   const cookieFeature = showPlpHeader ? 'greeting' : 'no-greeting';
   const cookie = `feature=${cookieFeature}; Path=/; Max-Age=60`;
+  const cookie2 = `variant=${variant}; Path=/; HttpOnly`;
 
-  if (showPlpHeader) {
-    const url = new URL(request.url);
-    url.pathname = '/api/welcome2';
-    return Response.redirect(url);
-  }
-  
+  const cookieString = request.headers.get('cookie'); 
+  console.log("🚀 ~ eventHandler ~ cookieString:", cookieString)
+  console.log("🚀 ~ eventHandler ~ cookie:", cookie)
+  const cookieArray = cookieString ? cookieString.split(';') : [];
+  console.log("🚀 ~ eventHandler ~ cookieArray:", cookieArray)
+  const newCookieArray = cookieArray.filter((cookie) => {
+    return !cookie.includes('feature');
+  });
+  console.log("🚀 ~ eventHandler ~ newCookieArray:", newCookieArray)
+  newCookieArray.push(cookie);
+  newCookieArray.push(cookie2);
+  console.log("🚀 ~ eventHandler ~ newCookieArray:", newCookieArray)
+  const newCookie = newCookieArray.join(';');
+  console.log("🚀 ~ eventHandler ~ newCookie:", newCookie)
+
+
   return new Response(null, {
     headers: {
-      'Set-Cookie': cookie,
+      'Set-Cookie': newCookie,
     },
   });
 }
