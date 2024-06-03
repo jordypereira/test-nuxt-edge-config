3dbc92c51835425655159034dfaadcf2c893286d
Jordy Pereira <dev@jordypereira.be>
2024-06-03 21:52:39 +0200

    show cookies

---
+++ components/showAllCookies.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>All Cookies</h1>
+    <ul>
+      <li v-for="cookie in allCookies" :key="cookie">{{ cookie }}</li>
+    </ul>
+  
+  </div>
+</template>
+
+<script lang="ts" setup>
+const allCookies = document.cookie.split(';').map(cookie => cookie.trim());
+</script>
+
+<style scoped>
+
+</style>
